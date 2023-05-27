c0c8c3d03c87939b0066065834a5dedde1d9ffb0
duongph03 <duongddph23612@fpt.edu.vn>
2023-05-27 21:02:44 +0700

    add  file img

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import Homepage from "./src/page/homepage";
 import Navigo from "navigo";
 import './assets/css/style.css'
+import './assets/img'
 const router = new Navigo("/", { linkSelector: "a" });
 const app = document.querySelector("#app");
 
--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from "vite";
 import path from "path";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 export default defineConfig({
     build: {
@@ -9,8 +10,13 @@ export default defineConfig({
             input: {
                 main: path.resolve(__dirname, "./index.html"),
                 css: path.resolve(__dirname, "/assets/css/style.css"),
-                img: path.resolve(__filename, "/assets/img")
             },
         },
     },
+    plugins: [
+        viteStaticCopy({
+            include: ['**/*.png'],
+            outputDir: 'dist/assets/img'
+        })
+    ]
 });
